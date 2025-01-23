4c3a3c1818f0e2c5246b249981f87e6f980d8db6
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-23 14:41:49 -0300

    add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const productosRoutes = require('./routes/productsRoutes.js');
 const usersRoutes = require('./routes/usersRoutes.js');
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors());
+app.get('/', (_req, res) => {
   res.send('Hola mundo');
 });
 
