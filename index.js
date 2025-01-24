da77bc47907ba77041468cadd99883294839730e
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-24 00:20:04 -0300

    modify index for new Routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const productosRoutes = require('./routes/productsRoutes.js');
 const usersRoutes = require('./routes/usersRoutes.js');
+const salesRoutes = require('./routes/salesRoutes.js');
+const salesItemsRoutes = require('./routes/salesItemsRoutes.js');
 const port = 3000;
 
 app.use(express.json());
@@ -14,6 +16,8 @@ app.get('/', (_req, res) => {
 //routes
 app.use('/products', productosRoutes);
 app.use('/users', usersRoutes);
+app.use('/sales', salesRoutes);
+app.use('/sales-items', salesItemsRoutes);
 
 
 app.listen(port, () => {
