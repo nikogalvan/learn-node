35be50a550bcab31ba2b1470283c54c186462b08
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-24 00:21:12 -0300

    update users controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,23 +1,85 @@
+const db = require('../database/conexion.js');
+
 class UsersController {
   constructor() {
   }
   consultar(_req, res) {
-    res.json({ msg: 'Consulta users desde clase' });
+    try {
+      db.query(`SELECT * FROM products`,
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(201).json(rows);
+        });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
   consultarDetalle(req, res) {
     const { id } = req.params;
-    res.json({ msg: `Consulta detalle del user id: ${id} desde clase` });
+    try {
+      db.query(`SELECT * FROM ecommerce.product WHERE product_id = ? `, [id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(200).json(rows[0]);
+        });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
-  ingresar(_req, res) {
-    res.json({ msg: 'Ingresado users desde clase' });
+  ingresar(req, res) {
+    try {
+      const { name, description, price, stock_quantity } = req.body;
+      db.query(`INSERT INTO ecommerce.product
+      (product_id, name, description, price, stock_quantity)
+      VALUES(NULL, ?,? ,?, ?);`,
+        [name, description, price, stock_quantity], (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          res.status(201).json({ id: rows.insertId });
+        });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
   eliminar(req, res) {
     const { id } = req.params;
-    res.json({ msg: `Eliminar user con id ${id} desde clase` });
+    try {
+      db.query(`DELETE FROM product WHERE product_id = ? `, [id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          if (rows.affectedRows === 1) {
+            res.status(200).json({ response: 'Deleted successfull' });
+          }
+        });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
+
   actualizar(req, res) {
-    const { id } = req.params;
-    res.json({ msg: `Actualizar user con id ${id} desde clase` });
+    try {
+      const { product_id, name, description, price, stock_quantity } = req.body;
+      db.query(`UPDATE product
+      SET name = ?, description = ?, price = ?, stock_quantity = ?
+      WHERE product_id = ?;`, [name, description, price, stock_quantity, product_id],
+        (err, rows) => {
+          if (err) {
+            res.status(400).send(err);
+          }
+          if (rows.affectedRows === 1) {
+            res.status(200).json({ response: 'Updated successfull' });
+          }
+        });
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 }
 
